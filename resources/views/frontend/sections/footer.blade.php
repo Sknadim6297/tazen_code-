86d09475ab853dbef3300653446d5805c6e35949
1999-Tania <tania.titli2017@gmail.com>
2025-06-04 18:48:16 +0530

    changes

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -5,13 +5,13 @@
                 <h3 data-bs-target="#collapse_1">Quick Links</h3>
                 <div class="collapse dont-collapse-sm links" id="collapse_1">
                     <ul>
-                        <li><a href="submit-professional.html">Join Us</a></li>
-                        <li><a href="help.html">Help</a></li>
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="contacts.html">Contacts</a></li>
-                        <li><a href="about-us.html">About Us</a></li>
-                        <li><a href="event-list.html">Events</a></li>
+                        <li><a href="{{ route('professional.login') }}">Join Us</a></li>
+                        <li><a href="#">Help</a></li>
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('blog.index') }}">Blog</a></li>
+                        <li><a href="{{ url('/contact') }}">Contacts</a></li>
+                        <li><a href="{{ url('/about') }}">About Us</a></li>
+                        <li><a href="{{ route('event.list') }}">Events</a></li>
                     </ul>
                 </div>
             </div>
@@ -44,11 +44,11 @@
                     <div class="follow_us">
                         <h5>Follow Us</h5>
                         <ul>
-                            <li><a href="#0"><i class="bi bi-facebook"></i></a></li>
-                            <li><a href="#0"><i class="bi bi-twitter-x"></i></a></li>
-                            <li><a href="#0"><i class="bi bi-instagram"></i></a></li>
-                            <li><a href="#0"><i class="bi bi-tiktok"></i></a></li>
-                            <li><a href="#0"><i class="bi bi-whatsapp"></i></a></li>
+                            <li><a href="https://www.facebook.com" target="_blank"><i class="bi bi-facebook"></i></a></li>
+                            <li><a href="https://twitter.com" target="_blank"><i class="bi bi-twitter-x"></i></a></li>
+                            <li><a href="https://www.instagram.com" target="_blank"><i class="bi bi-instagram"></i></a></li>
+                            <li><a href="https://www.tiktok.com" target="_blank"><i class="bi bi-tiktok"></i></a></li>
+                            <li><a href="https://wa.me/your_whatsapp_number" target="_blank"><i class="bi bi-whatsapp"></i></a></li>
                         </ul>
                     </div>
                 </div>
@@ -82,8 +82,8 @@
             </div>
             <div class="col-lg-6">
                 <ul class="additional_links">
-                    <li><a href="terms-condition.html">Terms and conditions</a></li>
-                    <li><a href="terms-condition.html">Privacy</a></li>
+                    <li><a href="#">Terms and conditions</a></li>
+                    <li><a href="#">Privacy</a></li>
                     <li><span>© Tazen</span></li>
                 </ul>
             </div>
